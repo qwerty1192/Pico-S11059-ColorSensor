12d4bbe0d5578c1d694d6a9eb3f8517c7c68ce84
Kawakira <167072405+Kawakira@users.noreply.github.com>
2024-04-18 18:11:13 +0900

    change init

--- S11059.h
+++ S11059.h
@@ -25,7 +25,17 @@ typedef struct
     uint8_t integration_time;
 } S11059_Settings;
 
-void s11059_init(i2c_inst_t *bus)
+void s11059_write_settings(i2c_inst_t *bus, S11059_Settings *settings)
+{
+    uint8_t buf[2];
+    buf[0] = 0x00;
+    buf[1] = 0b10000000 | ((settings->gain << 3) & 0b1000) | (settings->integration_time & 0b11);
+    i2c_write_blocking(bus, S11059_address, buf, 2, false);
+    buf[1] = ((settings->gain << 3) & 0b1000) | (settings->integration_time & 0b11);
+    i2c_write_blocking(bus, S11059_address, buf, 2, false);
+}
+
+void s11059_init(i2c_inst_t *bus, S11059_Settings *settings)
 {
     i2c_init(bus, 200 * 1000);
 
@@ -42,16 +52,7 @@ void s11059_init(i2c_inst_t *bus)
         gpio_pull_up(S11059_I2C1_SCL);
         bi_decl(bi_2pins_with_func(S11059_I2C1_SDA, S11059_I2C1_SCL, GPIO_FUNC_I2C));
     }
-}
-
-void s11059_write_settings(i2c_inst_t *bus, S11059_Settings *settings)
-{
-    uint8_t buf[2];
-    buf[0] = 0x00;
-    buf[1] = 0b10000000 | ((settings->gain << 3) & 0b1000) | (settings->integration_time & 0b11);
-    i2c_write_blocking(bus, S11059_address, buf, 2, false);
-    buf[1] = ((settings->gain << 3) & 0b1000) | (settings->integration_time & 0b11);
-    i2c_write_blocking(bus, S11059_address, buf, 2, false);
+    s11059_write_settings(bus, settings);
 }
 
 void s11059_read_data(i2c_inst_t *bus, uint16_t *red, uint16_t *green, uint16_t *blue, uint16_t *ir)
